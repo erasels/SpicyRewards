cd5e20bd638db7a339a9043784aec262b9b1ca68
erasels <emanuelbedburdick@yahoo.de>
2021-06-03 13:15:03 +0200

    Added general reward generation helper

--- SpicyRewards/src/main/java/SpicyRewards/challenges/AbstractChallenge.java
+++ SpicyRewards/src/main/java/SpicyRewards/challenges/AbstractChallenge.java
@@ -2,16 +2,23 @@ package SpicyRewards.challenges;
 
 import SpicyRewards.SpicyRewards;
 import SpicyRewards.rewards.AbstractSpicyReward;
+import SpicyRewards.rewards.HealReward;
+import SpicyRewards.rewards.MaxHpReward;
+import SpicyRewards.rewards.cardRewards.ModifiedCardReward;
+import SpicyRewards.rewards.selectCardsRewards.RemoveReward;
+import SpicyRewards.util.UC;
 import basemod.helpers.CardBorderGlowManager;
 import com.megacrit.cardcrawl.actions.utility.UseCardAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.core.AbstractCreature;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.localization.UIStrings;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import com.megacrit.cardcrawl.potions.AbstractPotion;
 import com.megacrit.cardcrawl.powers.AbstractPower;
 import com.megacrit.cardcrawl.rewards.RewardItem;
+import org.apache.commons.lang3.math.NumberUtils;
 
 import java.util.ArrayList;
 
@@ -26,6 +33,7 @@ public abstract class AbstractChallenge {
     public Type type;
 
     public enum Tier {EASY, NORMAL, HARD}
+
     public enum Type {NORMAL, OPTIN}
 
     public AbstractChallenge(String id, String text, String name, RewardItem r, Tier t, Type type) {
@@ -35,7 +43,7 @@ public abstract class AbstractChallenge {
         this.name = name;
         tier = t;
         this.type = type;
-        if(type == Type.OPTIN) {
+        if (type == Type.OPTIN) {
             done = true;
             shouldShowTip = true;
         }
@@ -44,9 +52,9 @@ public abstract class AbstractChallenge {
     }
 
     protected void initText() {
-        if(reward != null) {
+        if (reward != null) {
             rewardText = uiText.TEXT_DICT.get("prefix");
-            if(reward instanceof AbstractSpicyReward) {
+            if (reward instanceof AbstractSpicyReward) {
                 rewardText += ((AbstractSpicyReward) reward).getRewardText();
             } else {
                 rewardText += reward.text;
@@ -55,12 +63,13 @@ public abstract class AbstractChallenge {
     }
 
     public AbstractChallenge initReward() {
-        if(reward == null) {
+        if (reward == null) {
             rollReward();
         }
         initText();
         return this;
     }
+
     protected abstract void rollReward();
 
     public void initAtBattleStart() {
@@ -68,7 +77,7 @@ public abstract class AbstractChallenge {
     }
 
     public void complete() {
-        if(!failed) {
+        if (!failed) {
             SpicyRewards.challengeBtn.flash();
             done = true;
         }
@@ -95,42 +104,112 @@ public abstract class AbstractChallenge {
     }
 
     //Clean-up when this challenge is removed from the current challenges
-    public void onRemove() {}
+    public void onRemove() {
+    }
 
     protected abstract ArrayList<String> getExclusions();
-    public boolean isDone() {return !failed && done;}
+
+    public boolean isDone() {
+        return !failed && done;
+    }
 
     private void addCustomGlowInfo() {
         CardBorderGlowManager.GlowInfo info = getCustomGlowInfo();
-        if(info != null) {
+        if (info != null) {
             CardBorderGlowManager.addGlowInfo(info);
         }
     }
 
     private void removeCustomGlowInfo() {
         CardBorderGlowManager.GlowInfo info = getCustomGlowInfo();
-        if(info != null) {
+        if (info != null) {
             CardBorderGlowManager.removeGlowInfo(info);
         }
     }
 
-    protected CardBorderGlowManager.GlowInfo getCustomGlowInfo() {return null;}
+    protected CardBorderGlowManager.GlowInfo getCustomGlowInfo() {
+        return null;
+    }
 
-    public void atBattleStart() {}
-    public void onVictory() {}
-    public void onApplyPower(AbstractPower p, AbstractCreature target, AbstractCreature source) {}
-    public void atStartOfTurn() {}
-    public void atEndOfTurn() {}
-    public void onMonsterDeath(AbstractMonster m, boolean triggerRelics) {}
-    public void onUseCard(AbstractCard card, UseCardAction action) {}
-    public void onCardDraw(AbstractCard card) {}
-    public void onUsePotion(AbstractPotion p) {}
-    public void onDiscardPotion(AbstractPotion p) {}
+    public void atBattleStart() {
+    }
+
+    public void onVictory() {
+    }
+
+    public void onApplyPower(AbstractPower p, AbstractCreature target, AbstractCreature source) {
+    }
+
+    public void atStartOfTurn() {
+    }
+
+    public void atEndOfTurn() {
+    }
+
+    public void onMonsterDeath(AbstractMonster m, boolean triggerRelics) {
+    }
+
+    public void onUseCard(AbstractCard card, UseCardAction action) {
+    }
+
+    public void onCardDraw(AbstractCard card) {
+    }
+
+    public void onUsePotion(AbstractPotion p) {
+    }
+
+    public void onDiscardPotion(AbstractPotion p) {
+    }
+
+    protected RewardItem getRandomGeneralReward() {
+        int i = ChallengeSystem.challengeRng.random(3);
+        switch (tier) {
+            case HARD:
+                switch (i) {
+                    case 0:
+                        return new RewardItem(56);
+                    case 1:
+                        return new RewardItem(AbstractDungeon.returnRandomPotion(AbstractPotion.PotionRarity.RARE, false));
+                    case 2:
+                        return new RemoveReward();
+                    case 3:
+                        return new RewardItem(AbstractDungeon.returnRandomRelic(AbstractDungeon.returnRandomRelicTier()));
+                }
+                break;
+            case NORMAL:
+                switch (i) {
+                    case 0:
+                        return new RewardItem(32);
+                    case 1:
+                        return new RewardItem(AbstractDungeon.returnRandomPotion());
+                    case 2:
+                        return new RewardItem();
+                    case 3:
+                        return new MaxHpReward(6);
+                }
+                break;
+            case EASY:
+                switch (i) {
+                    case 0:
+                        return new RewardItem(18);
+                    case 1:
+                        return new RewardItem(AbstractDungeon.returnRandomPotion(AbstractPotion.PotionRarity.COMMON, false));
+                    case 2:
+                        //Small card reward
+                        return new ModifiedCardReward(null, null, -1, null, false, null);
+                    case 3:
+                        return new HealReward((int) NumberUtils.max(UC.p().maxHealth * 0.07f, 4));
+                }
+        }
+
+        SpicyRewards.logger.error("Unexpected Challenge tier encountered!");
+        return new RewardItem();
+    }
 
     public AbstractChallenge makeCopy() {
-        try{
+        try {
             return this.getClass().newInstance();
-        }catch(InstantiationException | IllegalAccessException e) {
+        } catch (InstantiationException | IllegalAccessException e) {
             throw new RuntimeException("Failed to auto-generate makeCopy for challenge: " + id);
         }
     }
