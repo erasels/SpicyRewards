fdbcbb3f6026116193ecbb74eb480257c57b74d3
erasels <emanuelbedburdick@yahoo.de>
2021-06-04 10:16:04 +0200

    Reworked DrawLose Challenge

--- SpicyRewards/src/main/java/SpicyRewards/challenges/optIn/DrawLoseCardChallenge.java
+++ SpicyRewards/src/main/java/SpicyRewards/challenges/optIn/DrawLoseCardChallenge.java
@@ -4,27 +4,40 @@ import SpicyRewards.SpicyRewards;
 import SpicyRewards.challenges.AbstractChallenge;
 import SpicyRewards.rewards.data.RetainCardReward;
 import SpicyRewards.util.UC;
-import com.megacrit.cardcrawl.actions.utility.LoseBlockAction;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.DamageAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.localization.UIStrings;
+import com.megacrit.cardcrawl.ui.panels.TopPanel;
 
 import java.util.ArrayList;
 
 public class DrawLoseCardChallenge extends AbstractChallenge {
     public static final String ID = SpicyRewards.makeID("DrawLoseCard");
     private static final UIStrings uiText = CardCrawlGame.languagePack.getUIString(ID + "Challenge");
-    private static final int AMT = 3;
+    private static final int AMT = 5;
+    private static final String replaceText = "[REMOVE THIS]";
+
+    private int drawCounter, drawLimit;
 
     protected static ArrayList<String> exclusions = new ArrayList<>();
 
     public DrawLoseCardChallenge() {
         super(ID,
-                fill(uiText.TEXT_DICT.get("desc"), AMT),
+                String.format(uiText.TEXT_DICT.get("desc"), replaceText, AMT),
                 uiText.TEXT_DICT.get("name"),
                 null,
-                Tier.EASY,
+                Tier.HARD,
                 Type.OPTIN);
+    }
+
+    @Override
+    protected void initText() {
+        drawLimit = UC.p().masterHandSize;
+        challengeText = challengeText.replace(replaceText, drawLimit + TopPanel.getOrdinalNaming(drawLimit));
+        super.initText();
     }
 
     @Override
@@ -34,7 +47,14 @@ public class DrawLoseCardChallenge extends AbstractChallenge {
 
     @Override
     public void onCardDraw(AbstractCard card) {
-        UC.atb(new LoseBlockAction(UC.p(), null, AMT));
+        if(++drawCounter > drawLimit) {
+            UC.atb(new DamageAction(UC.p(), new DamageInfo(null, AMT, DamageInfo.DamageType.THORNS), AbstractGameAction.AttackEffect.SLASH_HORIZONTAL, true));
+        }
+    }
+
+    @Override
+    public void atEndOfRound() {
+        drawCounter = 0;
     }
 
     @Override
