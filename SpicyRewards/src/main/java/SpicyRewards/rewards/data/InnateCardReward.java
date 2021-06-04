d3908b03e4112b9a95dc9544e256eb4aaa3f864d
erasels <emanuelbedburdick@yahoo.de>
2021-06-04 18:36:40 +0200

    Checked up upgrade innate in Innate Card Reward filter

--- SpicyRewards/src/main/java/SpicyRewards/rewards/data/InnateCardReward.java
+++ SpicyRewards/src/main/java/SpicyRewards/rewards/data/InnateCardReward.java
@@ -3,13 +3,24 @@ package SpicyRewards.rewards.data;
 import SpicyRewards.SpicyRewards;
 import SpicyRewards.rewards.cardRewards.ModifiedCardReward;
 import com.badlogic.gdx.graphics.Color;
+import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 
 public class InnateCardReward extends ModifiedCardReward {
     private static final String rewardText = CardCrawlGame.languagePack.getUIString(SpicyRewards.makeID("Rewards")).TEXT_DICT.get("innateCR");
 
     public InnateCardReward() {
-        super(Color.TAN, null,  0, null, false, c -> !(c.isInnate));
+        super(Color.TAN, null, 0, null, false, c -> {
+            if (c.isInnate)
+                return false;
+
+            if (!c.upgraded) {
+                AbstractCard c2 = c.makeCopy();
+                c2.upgrade();
+                return !c2.isInnate;
+            }
+            return true;
+        });
     }
 
     @Override
