b92070755e885aca8d29c64f0494f63fb9237202
erasels <emanuelbedburdick@yahoo.de>
2021-06-06 21:13:45 +0200

    Added different ctor to CustomRelicReward

--- SpicyRewards/src/main/java/SpicyRewards/rewards/CustomRelicReward.java
+++ SpicyRewards/src/main/java/SpicyRewards/rewards/CustomRelicReward.java
@@ -3,11 +3,13 @@ package SpicyRewards.rewards;
 import SpicyRewards.SpicyRewards;
 import SpicyRewards.util.UC;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.helpers.RelicLibrary;
 import com.megacrit.cardcrawl.relics.AbstractRelic;
 import com.megacrit.cardcrawl.relics.Circlet;
 import com.megacrit.cardcrawl.rewards.RewardItem;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class CustomRelicReward extends RewardItem {
     public CustomRelicReward(AbstractRelic relic) {
@@ -16,10 +18,25 @@ public class CustomRelicReward extends RewardItem {
             SpicyRewards.logger.info(String.format("Tried to remove %s from the %s-pool but it wasn't found. Potential duplicate?", relic.name, relic.tier.name()));
     }
 
+    public CustomRelicReward(String relicid) {
+        super(RelicLibrary.getRelic(relicid).makeCopy());
+        //relic is set in the super with relic input param
+        if(!removeRelicFromPool(relic, false) && relic.tier != AbstractRelic.RelicTier.SPECIAL)
+            SpicyRewards.logger.info(String.format("Tried to remove %s from the %s-pool but it wasn't found. Potential duplicate?", relic.name, relic.tier.name()));
+    }
+
     public CustomRelicReward(AbstractRelic relic, ArrayList<AbstractRelic> backups) {
         this(UC.p().hasRelic(relic.relicId)?
                 backups.stream().filter(r -> UC.p().hasRelic(r.relicId)).findAny().orElse(new Circlet()):
                 relic);
+    }
+
+    public CustomRelicReward(String... args) {
+        this(Arrays.stream(args).sequential().filter(s -> UC.p().hasRelic(s)).findFirst().orElse(Circlet.ID));
+    }
+
+    private static boolean removeRelicFromPool(String s, boolean allPools) {
+        return removeRelicFromPool(RelicLibrary.getRelic(s), allPools);
     }
 
     private static boolean removeRelicFromPool(AbstractRelic r, boolean allPools) {
