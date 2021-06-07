a6c75880b06ea21a61cd4f462713bcd81ac64e42
erasels <emanuelbedburdick@yahoo.de>
2021-06-07 19:34:14 +0200

    Revert "Update CustomRelicReward.java"
    This reverts commit cd2990dbee0534ca9990fac1d5db0c924d3c00e0.

--- SpicyRewards/src/main/java/SpicyRewards/rewards/CustomRelicReward.java
+++ SpicyRewards/src/main/java/SpicyRewards/rewards/CustomRelicReward.java
@@ -35,34 +35,34 @@ public class CustomRelicReward extends RewardItem {
         this(Arrays.stream(args).sequential().filter(s -> !UC.p().hasRelic(s)).findFirst().orElse(Circlet.ID));
     }
 
-    private static boolean removeRelicFromPool(AbstractRelic r, boolean allPools) {
-        return removeRelicFromPool(r.relicId, allPools);
+    private static boolean removeRelicFromPool(String s, boolean allPools) {
+        return removeRelicFromPool(RelicLibrary.getRelic(s), allPools);
     }
 
-    private static boolean removeRelicFromPool(String rid, boolean allPools) {
+    private static boolean removeRelicFromPool(AbstractRelic r, boolean allPools) {
         if(allPools) {
-            AbstractDungeon.bossRelicPool.removeIf(id -> id.equals(rid));
-            AbstractDungeon.shopRelicPool.removeIf(id -> id.equals(rid));
-            AbstractDungeon.rareRelicPool.removeIf(id -> id.equals(rid));
-            AbstractDungeon.uncommonRelicPool.removeIf(id -> id.equals(rid));
-            AbstractDungeon.commonRelicPool.removeIf(id -> id.equals(rid));
+            AbstractDungeon.bossRelicPool.removeIf(id -> id.equals(r.relicId));
+            AbstractDungeon.shopRelicPool.removeIf(id -> id.equals(r.relicId));
+            AbstractDungeon.rareRelicPool.removeIf(id -> id.equals(r.relicId));
+            AbstractDungeon.uncommonRelicPool.removeIf(id -> id.equals(r.relicId));
+            AbstractDungeon.commonRelicPool.removeIf(id -> id.equals(r.relicId));
         } else {
             if (r.tier != null) {
                 switch (r.tier) {
                     case COMMON:
-                        AbstractDungeon.commonRelicPool.removeIf(id -> id.equals(rid));
+                        AbstractDungeon.commonRelicPool.removeIf(id -> id.equals(r.relicId));
                         break;
                     case UNCOMMON:
-                        AbstractDungeon.uncommonRelicPool.removeIf(id -> id.equals(rid));
+                        AbstractDungeon.uncommonRelicPool.removeIf(id -> id.equals(r.relicId));
                         break;
                     case RARE:
-                        AbstractDungeon.rareRelicPool.removeIf(id -> id.equals(rid));
+                        AbstractDungeon.rareRelicPool.removeIf(id -> id.equals(r.relicId));
                         break;
                     case SHOP:
-                        AbstractDungeon.shopRelicPool.removeIf(id -> id.equals(rid));
+                        AbstractDungeon.shopRelicPool.removeIf(id -> id.equals(r.relicId));
                         break;
                     case BOSS:
-                        AbstractDungeon.bossRelicPool.removeIf(id -> id.equals(rid));
+                        AbstractDungeon.bossRelicPool.removeIf(id -> id.equals(r.relicId));
                         break;
                     default:
                         return false;
