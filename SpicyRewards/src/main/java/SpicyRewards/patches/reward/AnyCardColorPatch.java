f137242c68f06fe35d7caa15bb0f0191017dac0a
erasels <emanuelbedburdick@yahoo.de>
2021-06-07 07:17:53 +0200

    Fixed compendium crash with new basemod feature

--- SpicyRewards/src/main/java/SpicyRewards/patches/reward/AnyCardColorPatch.java
+++ SpicyRewards/src/main/java/SpicyRewards/patches/reward/AnyCardColorPatch.java
@@ -1,10 +1,12 @@
 package SpicyRewards.patches.reward;
 
+import basemod.patches.com.megacrit.cardcrawl.screens.compendium.CardLibraryScreen.NoLibraryType;
 import com.evacipated.cardcrawl.modthespire.lib.SpireEnum;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 
 public class AnyCardColorPatch {
     //Enum for color that makes any color card spawn like with prismatic shard
+    @NoLibraryType
     @SpireEnum
     public static AbstractCard.CardColor ANY;
 }
