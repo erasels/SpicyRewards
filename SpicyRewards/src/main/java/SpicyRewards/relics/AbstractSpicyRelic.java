730f640a17a7a1f06e59be1998ea143b966e7d71
erasels <emanuelbedburdick@yahoo.de>
2021-06-07 14:18:38 +0200

    Fixed relic textures not being found

--- SpicyRewards/src/main/java/SpicyRewards/relics/AbstractSpicyRelic.java
+++ SpicyRewards/src/main/java/SpicyRewards/relics/AbstractSpicyRelic.java
@@ -5,7 +5,8 @@ import SpicyRewards.util.TextureLoader;
 import basemod.abstracts.CustomRelic;
 
 public abstract class AbstractSpicyRelic extends CustomRelic {
+    private static final int idCutoff = SpicyRewards.makeID("").length();
     public AbstractSpicyRelic(String id, RelicTier tier, LandingSound sfx) {
-        super(id, TextureLoader.getTexture(SpicyRewards.makeRelicPath(id + ".png")), TextureLoader.getTexture(SpicyRewards.makeRelicPath("outline/" + id + ".png")), tier, sfx);
+        super(id, TextureLoader.getTexture(SpicyRewards.makeRelicPath(id.substring(idCutoff) + ".png")), TextureLoader.getTexture(SpicyRewards.makeRelicPath("outline/" + id.substring(idCutoff) + ".png")), tier, sfx);
     }
 }
