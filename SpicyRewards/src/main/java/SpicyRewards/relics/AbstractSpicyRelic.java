4b753decb7b3ea7c32be5ad65e25d54ca998ec2c
erasels <emanuelbedburdick@yahoo.de>
2021-06-08 17:01:09 +0200

    Fix relics not loading large images

--- SpicyRewards/src/main/java/SpicyRewards/relics/AbstractSpicyRelic.java
+++ SpicyRewards/src/main/java/SpicyRewards/relics/AbstractSpicyRelic.java
@@ -8,5 +8,6 @@ public abstract class AbstractSpicyRelic extends CustomRelic {
     private static final int idCutoff = SpicyRewards.makeID("").length();
     public AbstractSpicyRelic(String id, RelicTier tier, LandingSound sfx) {
         super(id, TextureLoader.getTexture(SpicyRewards.makeRelicPath(id.substring(idCutoff) + ".png")), TextureLoader.getTexture(SpicyRewards.makeRelicPath("outline/" + id.substring(idCutoff) + ".png")), tier, sfx);
+        largeImg = TextureLoader.getTexture(SpicyRewards.makeRelicPath("large/" + id.substring(idCutoff) + ".png"));
     }
 }
