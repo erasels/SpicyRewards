0f965d2490af8d2a34e315a56e2f0f69c907e2dd
erasels <emanuelbedburdick@yahoo.de>
2021-06-08 20:15:59 +0200

    DoomCalendar can only spawn post act 1 or in elite rooms

--- SpicyRewards/src/main/java/SpicyRewards/challenges/optIn/DoomCalendarChallenge.java
+++ SpicyRewards/src/main/java/SpicyRewards/challenges/optIn/DoomCalendarChallenge.java
@@ -13,9 +13,11 @@ import com.megacrit.cardcrawl.actions.GameActionManager;
 import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.cards.colorless.TheBomb;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.localization.UIStrings;
 import com.megacrit.cardcrawl.relics.MercuryHourglass;
 import com.megacrit.cardcrawl.relics.StoneCalendar;
+import com.megacrit.cardcrawl.rooms.MonsterRoomElite;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -23,6 +25,8 @@ import java.util.Arrays;
 public class DoomCalendarChallenge extends AbstractChallenge {
     public static final String ID = SpicyRewards.makeID("DoomCalendar");
     private static final UIStrings uiText = CardCrawlGame.languagePack.getUIString(SpicyRewards.makeID("DoomCalendarChallenge"));
+    //private static final HashMap<String, ArrayList<String>> weakMonstersPerDungeon = new HashMap<>();
+
     private static final int TURN = 7, AMOUNT = 36;
 
 
@@ -58,6 +62,36 @@ public class DoomCalendarChallenge extends AbstractChallenge {
         if(GameActionManager.turn == TURN) {
             UC.doDmg(UC.p(), AMOUNT, DamageInfo.DamageType.THORNS, AbstractGameAction.AttackEffect.LIGHTNING);
         }
+    }
+
+    //Can only spawn in elite fights
+    @Override
+    public boolean canSpawn() {
+        /*if(weakMonstersPerDungeon.get(AbstractDungeon.id) != null) {
+            //Something with monster info
+            //return AbstractDungeon.getCurrRoom().monsters.
+        } else {
+            try {
+                ClassPool pool = Loader.getClassPool();
+                CtClass ctClass = pool.get(CardCrawlGame.dungeon.getClass().getName());
+                CtMethod ctMethod = ctClass.getDeclaredMethod("generateWeakEnemies");
+
+                do {
+                    ctMethod.instrument(new ExprEditor() {
+                        @Override
+                        public void edit(NewExpr ne) {
+                            ne.
+
+                        }
+                    });
+                    ctClass = ctClass.getSuperclass();
+                } while (!ctClass.getName().equals(AbstractDungeon.class.getName()));
+            } catch (Exception ignored) {
+                weakMonstersPerDungeon.put(AbstractDungeon.id, null);
+            }
+        }*/
+
+        return AbstractDungeon.actNum > 1 || AbstractDungeon.getCurrRoom() instanceof MonsterRoomElite;
     }
 
     @Override
