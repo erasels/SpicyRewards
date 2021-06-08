d322a46fb665d992a520da41ff6cfe0ce9d74ae1
erasels <emanuelbedburdick@yahoo.de>
2021-06-08 22:20:03 +0200

    Added custom CardChoice icon

--- SpicyRewards/src/main/java/SpicyRewards/rewards/cardRewards/CardChoiceReward.java
+++ SpicyRewards/src/main/java/SpicyRewards/rewards/cardRewards/CardChoiceReward.java
@@ -1,19 +1,22 @@
 package SpicyRewards.rewards.cardRewards;
 
+import SpicyRewards.SpicyRewards;
 import SpicyRewards.patches.reward.NewRewardtypePatches;
 import SpicyRewards.rewards.AbstractSpicyReward;
+import SpicyRewards.util.TextureLoader;
 import SpicyRewards.util.UC;
+import com.badlogic.gdx.graphics.Texture;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
-import com.megacrit.cardcrawl.helpers.ImageMaster;
 
 import java.util.ArrayList;
 
 public class CardChoiceReward extends AbstractSpicyReward {
+    private static final Texture ICON = TextureLoader.getTexture(SpicyRewards.makeUIPath("choice.png"));
     public int cardPicks;
 
     public CardChoiceReward(int pickAmounts, ArrayList<AbstractCard> cards) {
-        super(ImageMaster.REWARD_CARD_NORMAL, TEXT[2], NewRewardtypePatches.SR_CARDCHOICEREWARD);
+        super(ICON, TEXT[2], NewRewardtypePatches.SR_CARDCHOICEREWARD);
         cardPicks = pickAmounts;
 
         this.cards.clear();
