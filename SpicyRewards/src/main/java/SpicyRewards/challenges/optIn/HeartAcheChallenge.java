666a4ffa1d0617e43607fa24630deba7c61e4bf6
erasels <emanuelbedburdick@yahoo.de>
2021-06-09 22:35:02 +0200

    Added HeartAche challenge

--- SpicyRewards/src/main/java/SpicyRewards/challenges/optIn/HeartAcheChallenge.java
+++ SpicyRewards/src/main/java/SpicyRewards/challenges/optIn/HeartAcheChallenge.java
@@ -8,37 +8,34 @@ import SpicyRewards.rewards.data.RetainCardReward;
 import SpicyRewards.rewards.selectCardsRewards.TransformReward;
 import SpicyRewards.util.UC;
 import com.megacrit.cardcrawl.actions.common.MakeTempCardInDrawPileAction;
-import com.megacrit.cardcrawl.cards.status.Slimed;
+import com.megacrit.cardcrawl.cards.status.*;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.core.Settings;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
-import com.megacrit.cardcrawl.helpers.CardLibrary;
 import com.megacrit.cardcrawl.localization.UIStrings;
 import com.megacrit.cardcrawl.potions.AbstractPotion;
-import com.megacrit.cardcrawl.relics.BagOfPreparation;
-import com.megacrit.cardcrawl.relics.ChemicalX;
-import com.megacrit.cardcrawl.relics.Omamori;
-import com.megacrit.cardcrawl.relics.Orichalcum;
+import com.megacrit.cardcrawl.relics.*;
 import com.megacrit.cardcrawl.rewards.RewardItem;
 
 import java.util.ArrayList;
 
-public class SlimeChallenge extends AbstractChallenge {
-    public static final String ID = SpicyRewards.makeID("Slimed");
+public class HeartAcheChallenge extends AbstractChallenge {
+    public static final String ID = SpicyRewards.makeID("HeartAche");
     private static final UIStrings uiText = CardCrawlGame.languagePack.getUIString(ID + "Challenge");
-    private static final int AMT = 6;
 
     protected static ArrayList<String> exclusions = new ArrayList<>();
 
-    public SlimeChallenge() {
+    public HeartAcheChallenge() {
         super(ID,
-                fill(uiText.TEXT_DICT.get("desc"), AMT),
+                uiText.TEXT_DICT.get("desc"),
                 uiText.TEXT_DICT.get("name"),
                 null,
-                Tier.NORMAL,
+                Tier.HARD,
                 Type.OPTIN);
         shouldShowTip = false;
     }
 
+    //TODO: Make rewards
     @Override
     protected void rollReward() {
         int i = ChallengeSystem.challengeRng.random(3);
@@ -47,7 +44,7 @@ public class SlimeChallenge extends AbstractChallenge {
                 reward = new RetainCardReward();
                 break;
             case 1:
-                reward = new CustomRelicReward(BagOfPreparation.ID, Orichalcum.ID, Omamori.ID, ChemicalX.ID);
+                reward = new CustomRelicReward(MedicalKit.ID);
                 break;
             case 2:
                 reward = new RewardItem(AbstractDungeon.returnRandomPotion(AbstractPotion.PotionRarity.RARE, false));
@@ -59,7 +56,11 @@ public class SlimeChallenge extends AbstractChallenge {
 
     @Override
     public void atBattleStart() {
-        UC.att(new MakeTempCardInDrawPileAction(CardLibrary.getCard(Slimed.ID), AMT, true, true));
+        UC.att(new MakeTempCardInDrawPileAction(new Dazed(), 1, true, false, false, Settings.WIDTH * 0.2F, Settings.HEIGHT / 2.0F));
+        UC.att(new MakeTempCardInDrawPileAction(new Slimed(), 1, true, false, false, Settings.WIDTH * 0.35F, Settings.HEIGHT / 2.0F));
+        UC.att(new MakeTempCardInDrawPileAction(new Wound(), 1, true, false, false, Settings.WIDTH * 0.5F, Settings.HEIGHT / 2.0F));
+        UC.att(new MakeTempCardInDrawPileAction(new Burn(), 1, true, false, false, Settings.WIDTH * 0.65F, Settings.HEIGHT / 2.0F));
+        UC.att(new MakeTempCardInDrawPileAction(new VoidCard(), 1, true, false, false, Settings.WIDTH * 0.8F, Settings.HEIGHT / 2.0F));
     }
 
     @Override
