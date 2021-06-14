b1e173ee3e20c3957934caf658f9049f93cedbae
erasels <emanuelbedburdick@yahoo.de>
2021-06-14 20:42:32 +0200

    Made RushC render turn counter

--- SpicyRewards/src/main/java/SpicyRewards/challenges/normal/RushChallenge.java
+++ SpicyRewards/src/main/java/SpicyRewards/challenges/normal/RushChallenge.java
@@ -3,20 +3,25 @@ package SpicyRewards.challenges.normal;
 import SpicyRewards.SpicyRewards;
 import SpicyRewards.challenges.AbstractChallenge;
 import SpicyRewards.challenges.ChallengeSystem;
+import SpicyRewards.challenges.IUIRenderChallenge;
 import SpicyRewards.challenges.optIn.DoomCalendarChallenge;
 import SpicyRewards.rewards.data.InnateCardReward;
 import SpicyRewards.rewards.data.UncommonCardReward;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.megacrit.cardcrawl.actions.GameActionManager;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.core.Settings;
+import com.megacrit.cardcrawl.helpers.FontHelper;
 import com.megacrit.cardcrawl.localization.UIStrings;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 
-public class RushChallenge extends AbstractChallenge {
+public class RushChallenge extends AbstractChallenge implements IUIRenderChallenge {
     public static final String ID = SpicyRewards.makeID("Rush");
     private static final UIStrings uiText = CardCrawlGame.languagePack.getUIString(ID + "Challenge");
-    private static final int TURN = 4;
+    private static final int TURN = 3;
 
     protected static ArrayList<String> exclusions = new ArrayList<>(Arrays.asList(DoomCalendarChallenge.ID));
 
@@ -43,14 +48,14 @@ public class RushChallenge extends AbstractChallenge {
 
     @Override
     public void atStartOfTurn() {
-        if(GameActionManager.turn >= TURN) {
+        if(GameActionManager.turn > TURN) {
             fail();
         }
     }
 
     @Override
     public void onVictory() {
-        if(GameActionManager.turn < TURN) {
+        if(GameActionManager.turn <= TURN) {
             complete();
         }
     }
@@ -58,5 +63,19 @@ public class RushChallenge extends AbstractChallenge {
     @Override
     protected ArrayList<String> getExclusions() {
         return exclusions;
+    }
+
+    //Only render turn counter if Doom challenge isn't a challenge and minty isn't loaded
+    @Override
+    public boolean shouldRender() {
+        return !SpicyRewards.hasMinty && ChallengeSystem.hasChallenge(DoomCalendarChallenge.ID);
+    }
+
+    @Override
+    public void renderUI(SpriteBatch sb, float xOffset, float curY) {
+        Color c = GameActionManager.turn < TURN? Settings.CREAM_COLOR : Settings.RED_TEXT_COLOR;
+        String s = String.format(uiText.TEXT_DICT.get("render"), GameActionManager.turn, TURN);
+        xOffset-= FontHelper.getWidth(FontHelper.panelNameFont, s, 1f);
+        FontHelper.renderFontLeft(sb, FontHelper.panelNameFont, s, xOffset, curY, c);
     }
 }
