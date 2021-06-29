b45828fc29c0463e31ffa639572858ab1c452372
erasels <emanuelbedburdick@yahoo.de>
2021-06-29 22:27:04 +0200

    Extended ExhaustCR search criteria

--- SpicyRewards/src/main/java/SpicyRewards/rewards/data/ExhuastCardReward.java
+++ SpicyRewards/src/main/java/SpicyRewards/rewards/data/ExhuastCardReward.java
@@ -4,12 +4,14 @@ import SpicyRewards.SpicyRewards;
 import SpicyRewards.rewards.cardRewards.ModifiedCardReward;
 import com.badlogic.gdx.graphics.Color;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.helpers.GameDictionary;
 
 public class ExhuastCardReward extends ModifiedCardReward {
     private static final String rewardText = CardCrawlGame.languagePack.getUIString(SpicyRewards.makeID("Rewards")).TEXT_DICT.get("exhaustCR");
+    private static final String exhaustString = GameDictionary.EXHAUST.NAMES[0].toLowerCase();
 
     public ExhuastCardReward() {
-        super(Color.DARK_GRAY, null,  0, null, false, c -> c.exhaust);
+        super(Color.DARK_GRAY, null,  0, null, false, c -> c.exhaust || c.rawDescription.toLowerCase().contains(exhaustString));
     }
 
     @Override
