eaa252ef973106ef736fa8f7991f429a994cc1ee
erasels <emanuelbedburdick@yahoo.de>
2021-07-18 20:44:13 +0200

    Added RelicStats to DowsingRod

--- SpicyRewards/src/main/java/SpicyRewards/relics/DowsingRod.java
+++ SpicyRewards/src/main/java/SpicyRewards/relics/DowsingRod.java
@@ -2,12 +2,14 @@ package SpicyRewards.relics;
 
 import SpicyRewards.SpicyRewards;
 import SpicyRewards.util.UC;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.common.RelicAboveCreatureAction;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 
-public class DowsingRod extends AbstractSpicyRelic{
+public class DowsingRod extends StatRelic{
     public static final String ID = SpicyRewards.makeID("DowsingRod");
+    private static final String STAT1 = "Block gained: ";
     public static final int BLK = 15;
 
     public DowsingRod() {
@@ -20,6 +22,13 @@ public class DowsingRod extends AbstractSpicyRelic{
             flash();
             UC.atb(new RelicAboveCreatureAction(UC.p(), this));
             UC.doDef(BLK, null, false);
+            UC.atb(new AbstractGameAction() {
+                @Override
+                public void update() {
+                    incrementStat(1, BLK);
+                    isDone = true;
+                }
+            });
         }
     }
 
