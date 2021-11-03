95a4d2817394dd2680b7772a23c95d7f1b6b687b
erasels <emanuelbedburdick@yahoo.de>
2021-11-03 14:59:23 +0100

    Added gold reward replacement for ExhaustCardReward

--- SpicyRewards/src/main/java/SpicyRewards/rewards/data/ExhuastCardReward.java
+++ SpicyRewards/src/main/java/SpicyRewards/rewards/data/ExhuastCardReward.java
@@ -4,7 +4,9 @@ import SpicyRewards.SpicyRewards;
 import SpicyRewards.rewards.cardRewards.ModifiedCardReward;
 import com.badlogic.gdx.graphics.Color;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.helpers.GameDictionary;
+import com.megacrit.cardcrawl.rewards.RewardItem;
 
 public class ExhuastCardReward extends ModifiedCardReward {
     private static final String rewardText = CardCrawlGame.languagePack.getUIString(SpicyRewards.makeID("Rewards")).TEXT_DICT.get("exhaustCR");
@@ -17,5 +19,10 @@ public class ExhuastCardReward extends ModifiedCardReward {
     @Override
     public String getRewardText() {
         return rewardText;
+    }
+
+    @Override
+    public RewardItem spawnReplacementReward(RewardItem previousReward) {
+        return new RewardItem(75 * (AbstractDungeon.actNum * 7));
     }
 }
