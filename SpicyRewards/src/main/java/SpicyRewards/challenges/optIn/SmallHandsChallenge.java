9807227175d21551e86e7ad60c6ccf1f68c437ed
erasels <emanuelbedburdick@yahoo.de>
2021-11-05 13:23:54 +0100

    Added SmallHandsChallenge

---
+++ SpicyRewards/src/main/java/SpicyRewards/challenges/optIn/SmallHandsChallenge.java
@@ -0,0 +1,80 @@
+package SpicyRewards.challenges.optIn;
+
+import SpicyRewards.SpicyRewards;
+import SpicyRewards.challenges.AbstractChallenge;
+import SpicyRewards.challenges.ChallengeSystem;
+import SpicyRewards.potions.RetainPotion;
+import SpicyRewards.relics.SpeedCharm;
+import SpicyRewards.rewards.CustomRelicReward;
+import SpicyRewards.rewards.data.BigButNoDrawCardReward;
+import SpicyRewards.rewards.data.NoDrawChoice;
+import SpicyRewards.rewards.data.RareCardReward;
+import SpicyRewards.rewards.data.RetainCardReward;
+import SpicyRewards.util.UC;
+import SpicyRewards.util.WidepotionDependencyHelper;
+import basemod.BaseMod;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.localization.UIStrings;
+import com.megacrit.cardcrawl.relics.MummifiedHand;
+import com.megacrit.cardcrawl.relics.QuestionCard;
+import com.megacrit.cardcrawl.relics.SnakeRing;
+import com.megacrit.cardcrawl.relics.SneckoEye;
+import com.megacrit.cardcrawl.rewards.RewardItem;
+import com.megacrit.cardcrawl.rooms.MonsterRoomElite;
+
+import java.util.ArrayList;
+
+public class SmallHandsChallenge extends AbstractChallenge {
+    public static final String ID = SpicyRewards.makeID("SmallHands");
+    private static final UIStrings uiText = CardCrawlGame.languagePack.getUIString(ID + "Challenge");
+    private static final int AMT = 4;
+
+    private int backupHandsize;
+
+    protected static ArrayList<String> exclusions = new ArrayList<>();
+
+    public SmallHandsChallenge() {
+        super(ID,
+                fill(uiText.TEXT_DICT.get("desc"), AMT),
+                uiText.TEXT_DICT.get("name"),
+                null,
+                Tier.HARD,
+                Type.OPTIN);
+        shouldShowTip = true;
+    }
+
+    @Override
+    protected void fillRewardList() {
+        rewardList.add(() -> new BigButNoDrawCardReward(), LOW_WEIGHT);
+        rewardList.add(() -> new NoDrawChoice(), LOW_WEIGHT);
+        rewardList.add(() -> new RetainCardReward(), LOW_WEIGHT);
+        rewardList.add(() -> new RareCardReward(), NORMAL_WEIGHT-1);
+        if(SpicyRewards.hasWidepots)
+            rewardList.add(() -> new RewardItem(WidepotionDependencyHelper.getWide(new RetainPotion())), NORMAL_WEIGHT);
+        if(!ChallengeSystem.spawnedRelicReward) {
+            if(AbstractDungeon.getCurrRoom() instanceof MonsterRoomElite) {
+                rewardList.add(() -> new CustomRelicReward(SpeedCharm.ID, SneckoEye.ID), NORMAL_WEIGHT + (UC.p().hasRelic(SpeedCharm.ID)? -2 : 2));
+            } else {
+                rewardList.add(() -> new CustomRelicReward(QuestionCard.ID, MummifiedHand.ID, SnakeRing.ID), NORMAL_WEIGHT);
+            }
+        }
+    }
+
+    @Override
+    public void atBattleStart() {
+        backupHandsize = BaseMod.MAX_HAND_SIZE - AMT;
+        BaseMod.MAX_HAND_SIZE = AMT;
+    }
+
+    @Override
+    public void onVictory() {
+        BaseMod.MAX_HAND_SIZE += backupHandsize;
+    }
+
+    @Override
+    protected ArrayList<String> getExclusions() {
+        return exclusions;
+    }
+}
+
