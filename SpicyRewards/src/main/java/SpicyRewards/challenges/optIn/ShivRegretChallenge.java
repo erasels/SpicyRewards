8d13da69d6a174fc2a06d41bae388708333779f2
erasels <emanuelbedburdick@yahoo.de>
2021-11-07 19:52:45 +0100

    Update ShivRegretChallenge.java

--- SpicyRewards/src/main/java/SpicyRewards/challenges/optIn/ShivRegretChallenge.java
+++ SpicyRewards/src/main/java/SpicyRewards/challenges/optIn/ShivRegretChallenge.java
@@ -9,6 +9,7 @@ import SpicyRewards.rewards.data.ScryCardReward;
 import SpicyRewards.rewards.selectCardsRewards.RemoveReward;
 import SpicyRewards.util.UC;
 import com.megacrit.cardcrawl.actions.common.MakeTempCardInDiscardAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.green.StormOfSteel;
 import com.megacrit.cardcrawl.cards.tempCards.Shiv;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
@@ -36,7 +37,7 @@ public class ShivRegretChallenge extends AbstractChallenge {
     protected void fillRewardList() {
         rewardList.add(() -> new SingleCardReward(UC.upgCard(new StormOfSteel())), UC.deck().findCardById(StormOfSteel.ID) != null ? LOW_WEIGHT : NORMAL_WEIGHT);
         rewardList.add(() -> new ScryCardReward(), NORMAL_WEIGHT);
-        rewardList.add(() -> new RemoveReward(), NORMAL_WEIGHT - 1);
+        rewardList.add(() -> new RemoveReward(AbstractCard.CardType.ATTACK, null), NORMAL_WEIGHT - 1);
         if(!ChallengeSystem.spawnedRelicReward)
             rewardList.add(() -> new CustomRelicReward(NinjaScroll.ID, Kunai.ID, Shuriken.ID, Sundial.ID, OrnamentalFan.ID), NORMAL_WEIGHT);
     }
